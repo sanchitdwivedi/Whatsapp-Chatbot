73fbc79fe315af0a412c258e5d5bfb5db542b49c
unknown <sanchitdwivedi123@gmail.com>
2021-08-31 21:55:53 +0530

    Test2

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ def detect_intent_from_text(msg, sender, language_code='en'):
     session = dialogflow_session_client.session_path(PROJECT_ID, sender)
     text_input = dialogflow.types.TextInput(text=msg, language_code=language_code)
     query_input = dialogflow.types.QueryInput(text=text_input)
-    response = dialogflow_session_client.detect_intent(session=sender, query_input=query_input)
+    response = dialogflow_session_client.detect_intent(session=session, query_input=query_input)
     return response.query_result
 
 def fetch_reply(msg, sender):
