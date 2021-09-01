441e4e0ee8c11e68753a0b907d5dbf89334d37df
unknown <sanchitdwivedi123@gmail.com>
2021-09-01 13:57:02 +0530

    Google results

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ import dialogflow_v2 as dialogflow
 dialogflow_session_client = dialogflow.SessionsClient()
 PROJECT_ID = "bot-new-rubr"
 
+from googlesearch import search
+
 def detect_intent_from_text(msg, sender, language_code='en'):
     session = dialogflow_session_client.session_path(PROJECT_ID, sender)
     text_input = dialogflow.types.TextInput(text=msg, language_code=language_code)
@@ -12,6 +14,15 @@ def detect_intent_from_text(msg, sender, language_code='en'):
     response = dialogflow_session_client.detect_intent(session=session, query_input=query_input)
     return response.query_result
 
+def getGoogleResult(parameters):
+    query = parameters.fields["query"]
+    return search(query.string_value, tld="co.in", num=1, stop=1, pause=1)
+
 def fetch_reply(msg, sender):
     response = detect_intent_from_text(msg, sender)
-    return response.fulfillment_text
+    if response.intent.display_name == 'Google search':
+        return "".join(getGoogleResult(response.parameters))
+        
+    else:
+        return response.fulfillment_text
+    
